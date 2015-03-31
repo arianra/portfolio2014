d6d654d5c42200123c2f44397ad65148865e8712
arian <dizzyone99@gmail.com>
2015-03-31 23:52:37 +0200

    more

--- app/contact.php
+++ app/contact.php
@@ -1,6 +1,5 @@
 <?php
-
-
+  header('Content-Type: application/json');
 
   $email = Trim( stripslashes( $_POST["email"] ) );
   $message = Trim( stripslashes( $_POST["message"] ) );
@@ -8,14 +7,9 @@
   $to_email = "hello@arianrazi.com";
 	$subject = "arianrazi.com -- Message from Contact Form";
 
-	$message =  "E-mail:\r\n" . $email . "\r\n\n"
-            . "Message:\r\n" . $message;
-
 	$headers =  "From: " . mysql_escape_string($email) . "\r\n"
 		        . "Reply-To: " . mysql_escape_string("hello@arianrazi.com") . "\r\n"
 		        . "X-Mailer: PHP/" . phpversion();
-
-  header('Content-Type: application/json');
 
 	if (!mail($to_email,$subject,$message,$headers))
 	{
@@ -25,5 +19,3 @@
 	{
 		echo json_encode(array('success' => '1'));
 	}
-
-?>
